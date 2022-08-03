eae4e6c35237460329cab4d46c5b56a3e91b34c5
AnthonyApollo <anthonyapollo@outlook.com>
2022-08-02 21:50:24 -0300

    feat: Make HomeTabBarController rootViewController

---
+++ Posterr/Modules/Home/HomeTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeTabBarController.swift
+//  Posterr
+//
+//  Created by Anthony Apollo on 02/08/22.
+//
+
+import Foundation
+import UIKit
+
+class HomeTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+    
+}
